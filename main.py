bf04acc7c13195c5121d300616dc37f07d05bcb8
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-01-26 14:49:37 +0100

    D: Added code to monitor frame per secondof game display

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()  # create a clock object
+    dt = 0  # variable that stores our delta time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = (
+            clock.tick(60) / 1000
+        )  # limit frame per second to 60 and gets delta time in seconds
 
 
 if __name__ == "__main__":
