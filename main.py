154cd5872d184e2dfc2ba7d3ed562155dd9e43af
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-01-30 04:47:54 +0100

    H: Grouped object into updatable and drawable per class methods

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()  # create a clock object
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0  # variable that stores our delta time
 
     while True:
@@ -14,9 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)  # limit frame per second to 60 and gets delta time in seconds
 
